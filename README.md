f56fd24759656383c2df96bde2de52278745bc82
Brandon Davis <neusfear@gmail.com>
2020-03-26 17:54:07 -0500

    Initialize repo with fabric-example-mod stuff

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-Campanion
+# Campanion
+
+## Info
+
+This mod is to add general camper companion items and utilities!
+
+## License
+
+This mod is available under the LGPLv3 license.
---
+++ src/main/java/campanion/Campanion.java
@@ -0,0 +1,14 @@
+package campanion;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Campanion implements ModInitializer {
+	@Override
+	public void onInitialize() {
+		// This code runs as soon as Minecraft is in a mod-load-ready state.
+		// However, some things (like resources) may still be uninitialized.
+		// Proceed with mild caution.
+
+		System.out.println("Hello Campanion world!");
+	}
+}
