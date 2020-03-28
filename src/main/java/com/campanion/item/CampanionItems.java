9d52f84c0e3b21f65a1a3efee5fdbc1d127986a6
Prospector <prospectordev@gmail.com>
2020-03-27 17:27:52 -0700

    Add Rope and MRE items

--- src/main/java/com/campanion/item/CampanionItems.java
+++ src/main/java/com/campanion/item/CampanionItems.java
@@ -1,7 +1,9 @@
 package com.campanion.item;
 
 import com.campanion.Campanion;
+import net.minecraft.item.FoodComponent;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 
@@ -11,6 +13,9 @@ import java.util.Map;
 public class CampanionItems {
 
 	private static final Map<Identifier, Item> ITEMS = new HashMap<>();
+
+	public static final Item ROPE = add("rope", new Item(new Item.Settings().group(ItemGroup.MATERIALS)));
+	public static final Item MRE = add("mre", new Item(new Item.Settings().group(ItemGroup.FOOD).food(new FoodComponent.Builder().hunger(8).saturationModifier(0.8F).build())));
 
 	private static <I extends Item> I add(String name, I item) {
 		ITEMS.put(new Identifier(Campanion.MOD_ID, name), item);
