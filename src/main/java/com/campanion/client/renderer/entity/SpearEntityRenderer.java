739fb74e549c9bc20770ce14dfbe3168f351d0e3
Wyn-Price <wynprice999@gmail.com>
2020-03-28 07:33:07 +0000

    Fixed spears not spawning on client

--- src/main/java/com/campanion/client/renderer/entity/SpearEntityRenderer.java
+++ src/main/java/com/campanion/client/renderer/entity/SpearEntityRenderer.java
@@ -15,18 +15,19 @@ import net.minecraft.util.math.MathHelper;
 
 public class SpearEntityRenderer extends EntityRenderer<SpearEntity> {
 	public static final Identifier SKIN = new Identifier("textures/entity/trident.png");
-	private final TridentEntityModel model = new TridentEntityModel();
+	private final SpearEntityModel model = new SpearEntityModel();
 
 	public SpearEntityRenderer(EntityRenderDispatcher entityRenderDispatcher) {
 		super(entityRenderDispatcher);
 	}
 
 	public void render(SpearEntity spear, float f, float g, MatrixStack matrixStack, VertexConsumerProvider vertexConsumerProvider, int i) {
+		SpearEntityModel model = new SpearEntityModel();
 		matrixStack.push();
 		matrixStack.multiply(Vector3f.POSITIVE_Y.getDegreesQuaternion(MathHelper.lerp(g, spear.prevYaw, spear.yaw) - 90.0F));
 		matrixStack.multiply(Vector3f.POSITIVE_Z.getDegreesQuaternion(MathHelper.lerp(g, spear.prevPitch, spear.pitch) + 90.0F));
-		VertexConsumer vertexConsumer = ItemRenderer.getArmorVertexConsumer(vertexConsumerProvider, this.model.getLayer(this.getTexture(spear)), false, spear.method_23751());
-		this.model.render(matrixStack, vertexConsumer, i, OverlayTexture.DEFAULT_UV, 1.0F, 1.0F, 1.0F, 1.0F);
+		VertexConsumer vertexConsumer = ItemRenderer.getArmorVertexConsumer(vertexConsumerProvider, model.getLayer(this.getTexture(spear)), false, spear.method_23751());
+		model.render(matrixStack, vertexConsumer, i, OverlayTexture.DEFAULT_UV, 1.0F, 1.0F, 1.0F, 1.0F);
 		matrixStack.pop();
 		super.render(spear, f, g, matrixStack, vertexConsumerProvider, i);
 	}
