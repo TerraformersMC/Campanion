7d27d5fddc22983f6021ffc269657a60edcf86b7
Prospector <prospectordev@gmail.com>
2020-03-28 03:35:10 -0700

    Make rope ladders climbable (Add RedstoneParadox's unmerged climbable api PR)

--- src/main/java/com/campanion/block/RopeLadderBlock.java
+++ src/main/java/com/campanion/block/RopeLadderBlock.java
@@ -16,11 +16,19 @@ import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.IWorld;
 import net.minecraft.world.World;
 import net.minecraft.world.WorldView;
+import net.redstoneparadox.climbable.api.event.block.ClimbingCallback;
 
 public class RopeLadderBlock extends LadderBlock {
 
 	public RopeLadderBlock(Settings settings) {
 		super(settings);
+		ClimbingCallback.EVENT.register((climber, state, pos) -> {
+			if (state.getBlock() == this) {
+				return 0.4F;
+			} else {
+				return Double.NaN;
+			}
+		});
 	}
 
 	@Override
@@ -51,7 +59,11 @@ public class RopeLadderBlock extends LadderBlock {
 				if (upperState.getBlock() != this && !upperState.isAir()) {
 					return ActionResult.FAIL;
 				}
+				ItemStack previousStack = stackInHand.copy();
 				ActionResult actionResult = stackInHand.useOnBlock(new ItemUsageContext(player, hand, new BlockHitResult(Vec3d.ZERO, Direction.UP, attemptPos, false)));
+				if (player.abilities.creativeMode && stackInHand == player.getStackInHand(hand) && stackInHand.getCount() < previousStack.getCount()) {
+					stackInHand.setCount(previousStack.getCount());
+				}
 				if (actionResult.isAccepted()) {
 					return actionResult;
 				}
---
+++ src/main/java/net/redstoneparadox/climbable/api/event/block/ClimbingCallback.java
@@ -0,0 +1,57 @@
+/*
+ * Copyright (c) 2016, 2017, 2018, 2019 FabricMC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package net.redstoneparadox.climbable.api.event.block;
+
+import net.minecraft.block.BlockState;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.util.math.BlockPos;
+
+import net.fabricmc.fabric.api.event.Event;
+import net.fabricmc.fabric.api.event.EventFactory;
+
+@FunctionalInterface
+public interface ClimbingCallback {
+	Event<ClimbingCallback> EVENT = EventFactory.createArrayBacked(ClimbingCallback.class,
+			listeners -> (entity, blockState, pos) -> {
+				double finalSpeed = Double.NaN;
+
+				for (ClimbingCallback listener : listeners) {
+					double speed = listener.canClimb(entity, blockState, pos);
+					if (!Double.isNaN(speed)) finalSpeed = speed;
+				}
+
+				return finalSpeed;
+			});
+
+	/**
+	 * Used for applying a non-vanilla climbing speed to the passed living entity.
+	 *
+	 * <p>The climbing speed returned determines how fast or slow the
+	 * Living Entity will climb. Can be {@link Double#NaN} if you
+	 * don't want to handle the given situation.</p>
+	 *
+	 * <p>In the event that all callbacks return null; vanilla's default behavior
+	 * will be applied. </p>
+	 *
+	 * @param climber The LivingEntity attempting to climb the block.
+	 * @param state The BlockState of the block that the climber is attempting to climb.
+	 * @param pos The BlockPos of the BlockState.
+	 *
+	 * @return The desired climbing speed or {@link Double#NaN}.
+	 */
+	double canClimb(LivingEntity climber, BlockState state, BlockPos pos);
+}
