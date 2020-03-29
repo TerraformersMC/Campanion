3c553ca4b36853c1961b57d1f3d631e3d8f4d133
Brandon Davis <neusfear@gmail.com>
2020-03-29 18:30:20 -0500

    try again

---
+++ src/main/java/com/campanion/item/MarshmellowOnAStickItem.java
@@ -0,0 +1,15 @@
+package com.campanion.item;
+
+import net.minecraft.item.Item;
+
+public class MarshmellowOnAStickItem extends Item {
+
+	private int type;
+
+	public MarshmellowOnAStickItem(Settings settings, int type) {
+		super(settings);
+		this.type = type;
+	}
+
+
+}
