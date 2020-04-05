a7ac77d1685454a5e0666236e41f7580ad51a408
wynprice999@gmail.com <wynprice999@gmail.com>
2020-04-05 06:40:35 +0100

    Fixed spear rendering from merge

--- src/main/java/com/campanion/client/items/SpearItemRenderer.java
+++ src/main/java/com/campanion/client/items/SpearItemRenderer.java
@@ -9,6 +9,8 @@ import net.minecraft.client.render.item.ItemRenderer;
 import net.minecraft.client.render.model.BakedModel;
 import net.minecraft.client.render.model.json.ModelTransformation;
 import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.client.util.math.Vector3f;
+import net.minecraft.entity.LivingEntity;
 import net.minecraft.item.ItemStack;
 
 public enum SpearItemRenderer {
@@ -16,26 +18,32 @@ public enum SpearItemRenderer {
 
     private final SpearEntityModel spearEntityModel = new SpearEntityModel();
 
-    public boolean render(ItemStack stack, ModelTransformation.Mode renderMode, boolean leftHanded, MatrixStack matrices, VertexConsumerProvider vertexConsumers, int light, int overlay, BakedModel model) {
-        boolean renderNormal = renderMode == ModelTransformation.Mode.GUI || renderMode == ModelTransformation.Mode.GROUND || renderMode == ModelTransformation.Mode.FIXED;
+    public boolean render(LivingEntity entity, ItemStack stack, ModelTransformation.Mode renderMode, boolean leftHanded, MatrixStack matrices, VertexConsumerProvider vertexConsumers, int light, int overlay, BakedModel model) {
+        if(renderMode == ModelTransformation.Mode.GUI || renderMode == ModelTransformation.Mode.GROUND || renderMode == ModelTransformation.Mode.FIXED) {
+            return false;
+        }
 
         matrices.push();
 
         model.getTransformation().getTransformation(renderMode).apply(leftHanded, matrices);
-        matrices.translate(-0.5D, -0.5D, -0.5D);
 
-        if (!renderNormal) {
-            matrices.scale(1.0F, -1.0F, -1.0F);
-            VertexConsumer spear = ItemRenderer.getArmorVertexConsumer(
-                vertexConsumers,
-                this.spearEntityModel.getLayer(SpearEntityRenderer.getTexture(((SpearItem)stack.getItem()).getType())),
-                false,
-                stack.hasEnchantmentGlint()
-            );
-            this.spearEntityModel.render(matrices, spear, light, overlay, 1.0F, 1.0F, 1.0F, 1.0F);
+        if(entity != null && entity.isUsingItem() && entity.getActiveItem() == stack && (renderMode == ModelTransformation.Mode.THIRD_PERSON_LEFT_HAND || renderMode == ModelTransformation.Mode.THIRD_PERSON_RIGHT_HAND)) {
+            matrices.multiply(Vector3f.POSITIVE_X.getDegreesQuaternion(180));
+            matrices.translate(0, 1.6, 0);
+        } else {
+            matrices.translate(0, 0.35, 0);
         }
 
+        matrices.scale(1.0F, -1.0F, -1.0F);
+        VertexConsumer spear = ItemRenderer.getArmorVertexConsumer(
+            vertexConsumers,
+            this.spearEntityModel.getLayer(SpearEntityRenderer.getTexture(((SpearItem)stack.getItem()).getType())),
+            false,
+            stack.hasEnchantmentGlint()
+        );
+        this.spearEntityModel.render(matrices, spear, light, overlay, 1.0F, 1.0F, 1.0F, 1.0F);
+
         matrices.pop();
-        return !renderNormal;
+        return true;
     }
 }
--- src/main/java/com/campanion/mixin/client/MixinItemRenderer.java
+++ src/main/java/com/campanion/mixin/client/MixinItemRenderer.java
@@ -11,8 +11,10 @@ import net.minecraft.client.render.item.ItemRenderer;
 import net.minecraft.client.render.model.BakedModel;
 import net.minecraft.client.render.model.json.ModelTransformation;
 import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.entity.LivingEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -21,9 +23,10 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(ItemRenderer.class)
 public class MixinItemRenderer {
 
-    @Inject(method = "renderItem", at = @At("HEAD"), cancellable = true)
-    public void renderItem(ItemStack stack, ModelTransformation.Mode renderMode, boolean leftHanded, MatrixStack matrices, VertexConsumerProvider vertexConsumers, int light, int overlay, BakedModel model, CallbackInfo info) {
-        if(stack.getItem() instanceof SpearItem && SpearItemRenderer.INSTANCE.render(stack, renderMode, leftHanded, matrices, vertexConsumers, light, overlay, model)) {
+    @Inject(method = "renderItem(Lnet/minecraft/entity/LivingEntity;Lnet/minecraft/item/ItemStack;Lnet/minecraft/client/render/model/json/ModelTransformation$Mode;ZLnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumerProvider;Lnet/minecraft/world/World;II)V", at = @At("HEAD"), cancellable = true)
+    public void renderItem(LivingEntity entity, ItemStack stack, ModelTransformation.Mode renderMode, boolean leftHanded, MatrixStack matrices, VertexConsumerProvider vertexConsumers, World world, int light, int overlay, CallbackInfo info) {
+        BakedModel model = MinecraftClient.getInstance().getItemRenderer().getHeldItemModel(stack, world, entity);
+        if(stack.getItem() instanceof SpearItem && SpearItemRenderer.INSTANCE.render(entity, stack, renderMode, leftHanded, matrices, vertexConsumers, light, overlay, model)) {
             info.cancel();
         }
         if(stack.getItem() == CampanionItems.TENT_BAG && renderMode != ModelTransformation.Mode.GUI) {
