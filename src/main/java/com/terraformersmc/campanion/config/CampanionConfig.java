cc9a2d166e238a9c2efc36736a4b66d3aacbe5a9
hugeblank <hugeblank@gmail.com>
2021-06-01 00:06:27 -0700

    Add renderTentPlacement option
    Will not render the tent - keeps the existing invalid overlay

--- src/main/java/com/terraformersmc/campanion/config/CampanionConfig.java
+++ src/main/java/com/terraformersmc/campanion/config/CampanionConfig.java
@@ -3,6 +3,7 @@ package com.terraformersmc.campanion.config;
 public class CampanionConfig {
 
 	public boolean renderTentContents = true;
+	public boolean renderTentPlacement = true;
 
 	public CampanionConfig() {}
 
