ea46283634449f2ac13a2d3073d51413d1f17da4
Jacob <jbuchanan21@qub.ac.uk>
2021-07-05 13:04:57 +0100

    Build and run for production

--- src/main/java/com/terraformersmc/campanion/mixin/client/MixinScreen.java
+++ src/main/java/com/terraformersmc/campanion/mixin/client/MixinScreen.java
@@ -17,11 +17,8 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(Screen.class)
 public abstract class MixinScreen {
-	@Shadow
-	public abstract void init(MinecraftClient client, int width, int height);
-
-	@Inject(method = "addDrawableChild", at = @At("HEAD"))
-	private <T extends Element & Drawable & Selectable> void onAddDrawableChild(T button, CallbackInfoReturnable<T> info) {
+	@Inject(method = "addDrawableChild(Lnet/minecraft/client/gui/Element;)Lnet/minecraft/client/gui/Element;", at = @At("HEAD"))
+	private void onAddDrawableChild(Element button, CallbackInfoReturnable<Element> info) {
 		if ((Object) this instanceof SleepingChatScreen && SleepingBagItem.getUsingStack(MinecraftClient.getInstance().player).isPresent()) {
 			if (button instanceof ClickableWidget) {
 				((ClickableWidget) button).setMessage(new TranslatableText("item.campanion.sleeping_bag.stop_sleeping"));
