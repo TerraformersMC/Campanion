6b9fee29e41669f561af888e69a6b09b53edd166
Jacob <jbuchanan21@qub.ac.uk>
2022-01-09 20:47:12 +0000

    Optional config support for backpacks in trinkets mod back slot

--- src/main/java/com/terraformersmc/campanion/network/C2SOpenBackpack.java
+++ src/main/java/com/terraformersmc/campanion/network/C2SOpenBackpack.java
@@ -2,10 +2,14 @@ package com.terraformersmc.campanion.network;
 
 import com.terraformersmc.campanion.Campanion;
 import com.terraformersmc.campanion.backpack.BackpackContainerFactory;
+import com.terraformersmc.campanion.config.CampanionConfigManager;
 import com.terraformersmc.campanion.item.BackpackItem;
+import dev.emi.trinkets.api.TrinketComponent;
+import dev.emi.trinkets.api.TrinketsApi;
 import io.netty.buffer.Unpooled;
 import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
 import net.fabricmc.fabric.api.networking.v1.PacketSender;
+import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.entity.EquipmentSlot;
 import net.minecraft.item.ItemStack;
 import net.minecraft.network.Packet;
@@ -25,6 +29,16 @@ public class C2SOpenBackpack {
 	public static void onPacket(MinecraftServer server, ServerPlayerEntity player, ServerPlayNetworkHandler networkHandler, PacketByteBuf buffer, PacketSender sender) {
 		server.execute(() -> {
 			ItemStack stack = player.getEquippedStack(EquipmentSlot.CHEST);
+
+			if (CampanionConfigManager.getConfig().isTrinketsBackpacksEnabled() &&
+					FabricLoader.getInstance().isModLoaded("trinkets")) {
+				TrinketComponent component = TrinketsApi.getTrinketComponent(player).orElse(null);
+
+				if (component != null && component.isEquipped(itemStack -> itemStack.getItem() instanceof BackpackItem)) {
+					stack = component.getEquipped(itemStack -> itemStack.getItem() instanceof BackpackItem).get(0).getRight();
+				}
+			}
+
 			if (stack.getItem() instanceof BackpackItem) {
 				BackpackItem.Type type = ((BackpackItem) stack.getItem()).type;
 				player.openHandledScreen(new BackpackContainerFactory(type));
